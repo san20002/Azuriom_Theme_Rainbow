198236b56a11768d37107c01de39a1c8242cd9a7
Latshow <latchere.y@gmail.com>
2021-02-12 10:25:16 +0100

    Clean config default

--- config/rules.php
+++ config/rules.php
@@ -2,18 +2,18 @@
 
 return [
     'discord-id' => ['nullable', 'string'],
-    'footer_description' => ['nullable', 'string'],
+    'headerIcons' => ['nullable', 'string'],
+    'headerVisibilityText' => ['nullable', 'string'],
     'sliders' => ['nullable', 'array'],
-    'footer_links' => ['nullable', 'array'],
     'articleLike' => ['nullable', 'string'],
     'articleComment' => ['nullable', 'string'],
-    'headerIcons' => ['nullable', 'string'],
     'voteIcons' => ['nullable', 'string'],
-    'headerVisibilityText' => ['nullable', 'string'],
     'cgu' => ['nullable', 'string'],
     'cgu_link' => ['nullable', 'string'],
     'cgv' => ['nullable', 'string'],
     'cgv_link' => ['nullable', 'string'],
     'download' => ['nullable', 'string'],
     'download_link' => ['nullable', 'string'],
+    'footer_description' => ['nullable', 'string'],
+    'footer_links' => ['nullable', 'array'],
 ];
